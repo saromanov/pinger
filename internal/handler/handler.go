351fbcb7b65bfb023dcf73712b90f842df842e24
Sergey <xxsmotur@gmail.com>
2019-03-20 17:50:10 +0500

    Add method for make new handler

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -2,9 +2,25 @@
 // Its getting models and validate it and store
 package handler
 
-import "github.com/saromanov/pinger/internal/storage"
+import (
+	"errors"
+	"github.com/saromanov/pinger/internal/storage"
+)
+
+var errNoStorage = errors.New("storage is not defined")
 
 // Handler represents part for operation with db
 type Handler struct {
-	Storage storage.Storage
+	Storage *storage.Storage
+}
+
+// New provides initialization of the handler
+func New(s *storage.Storage) (*Handler, error) {
+	if s == nil {
+		return nil, errNoStorage
+	}
+
+	return &Handler{
+		Storage: s,
+	}, nil
 }
