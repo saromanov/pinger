57e27f0ad81c1f5c95c622027b98456089011f54
Sergey <xxsmotur@gmail.com>
2019-03-20 18:21:04 +0500

    Add helpful method for site finding

--- internal/storage/site.go
+++ internal/storage/site.go
@@ -8,15 +8,27 @@ import (
 // GetSitesRequest provides argument with params
 // for find sites
 type GetSitesRequest struct {
-	Limit  uint
-	Offset uint
+	Limit  int
+	Offset int
 	UserID string
 }
 
 // GetSites returns list of sites
 func (s *Storage) GetSites(req GetSitesRequest) ([]*models.Site, error) {
+	if req.UserID == "" {
+		return s.getAllSites(req)
+	}
 	var sites []*models.Site
-	err := s.db.Where(nil).Find(&sites).Error
+	err := s.db.Where("user_id = ?", req.UserID).Find(&sites).Error
+	if err != nil {
+		return nil, errors.Wrap(err, "storage: unable to find sites")
+	}
+	return sites, nil
+}
+
+func (s *Storage) getAllSites(req GetSitesRequest) ([]*models.Site, error) {
+	var sites []*models.Site
+	err := s.db.Limit(req.Limit).Offset(req.Offset).Find(&sites).Error
 	if err != nil {
 		return nil, errors.Wrap(err, "storage: unable to find sites")
 	}
