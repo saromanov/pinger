5533531cf752bc239631aecfe2d2df4f10e8fdd6
Sergey <xxsmotur@gmail.com>
2019-03-22 11:40:26 +0500

    implementation of the ping method

---
+++ internal/core/ping.go
@@ -0,0 +1,27 @@
+package core
+
+import (
+	"errors"
+	"fmt"
+
+	goping "github.com/sparrc/go-ping"
+)
+
+var errSiteNotAvailable = errors.New("site is not available")
+
+// ping provides sending..
+func ping(url string) error {
+	pinger, err := goping.NewPinger(url)
+	if err != nil {
+		return fmt.Errorf("ping: unable to ping site %s: %v", url, err)
+	}
+
+	pinger.Count = 3
+	pinger.Run()
+	stats := pinger.Statistics()
+	if stats.PacketsRecv == 0 {
+		return errSiteNotAvailable
+	}
+
+	return nil
+}
