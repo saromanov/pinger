d3530d90066f7d5bb1d67b6c02b7629159c2ece1
Sergey <xxsmotur@gmail.com>
2019-03-22 11:33:38 +0500

    Add puppet for the core file

--- internal/core/core.go
+++ internal/core/core.go
@@ -1,3 +1,25 @@
 // Package core defines main logic of the pinger
 // it contains worker for checking site availability
 package core
+
+import (
+	"fmt"
+
+	"github.com/robfig/cron"
+)
+
+// Core defines struct for main logic
+type Core struct {
+}
+
+// checker gets records from db and checks availability
+func (c *Core) checker() {
+
+}
+
+// New provides initialization of the core
+func New() {
+	c := cron.New()
+	c.AddFunc("@every 1s", func() { fmt.Println("Every 1 second") })
+	c.Start()
+}
