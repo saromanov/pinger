f8432599a8296c1842daead2992903b31e970fd7
Sergey <xxsmotur@gmail.com>
2019-03-23 21:24:55 +0500

    Add separate method for start cron

--- internal/core/core.go
+++ internal/core/core.go
@@ -6,10 +6,12 @@ import (
 	"fmt"
 
 	"github.com/robfig/cron"
+	"github.com/saromanov/pinger/internal/handler"
 )
 
 // Core defines struct for main logic
 type Core struct {
+	hand *handler.Handler
 }
 
 // checker gets records from db and checks availability
@@ -17,9 +19,16 @@ func (c *Core) checker() {
 
 }
 
-// New provides initialization of the core
-func New() {
+func (core *Core) startCron() {
 	c := cron.New()
 	c.AddFunc("@every 1s", func() { fmt.Println("Every 1 second") })
 	c.Start()
+}
+
+// New provides initialization of the core
+func New(h *handler.Handler) {
+	c := &Core{
+		hand: h,
+	}
+	c.startCron()
 }
