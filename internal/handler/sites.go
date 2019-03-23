f3678e97b86cb0681b2b54bcc50939be3dfb6dd6
Sergey <xxsmotur@gmail.com>
2019-03-23 20:20:47 +0500

    Add struct for sending request to the site

--- internal/handler/sites.go
+++ internal/handler/sites.go
@@ -3,12 +3,16 @@ package handler
 import (
 	"github.com/pkg/errors"
 	"github.com/saromanov/pinger/internal/models"
+	"github.com/saromanov/pinger/internal/storage"
 	pb "github.com/saromanov/pinger/proto"
 )
 
 // GetSites returns list of the sites based on request
-func (h *Handler) GetSites(u *models.Account) ([]*pb.Site, error) {
-	sites, err := h.Storage.GetSites()
+func (h *Handler) GetSites(r *pb.GetSitesRequest) ([]*pb.Site, error) {
+	sites, err := h.Storage.GetSites(storage.GetSitesRequest{
+		Limit:  int(r.Limit),
+		Offset: int(r.Offset),
+	})
 	if err != nil {
 		return nil, errors.Wrap(err, "unable to get sites")
 	}
