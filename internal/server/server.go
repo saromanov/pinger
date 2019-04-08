56f35c113c8e72fbc2ffc555b9ddb8bfb3d764a1
Sergey <xxsmotur@gmail.com>
2019-04-08 21:25:41 +0500

    Split server on sites file

--- internal/server/server.go
+++ internal/server/server.go
@@ -87,47 +87,6 @@ func (s *server) getAccount(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
-// createSite makes a new site for user
-func (s *server) createSite(w http.ResponseWriter, r *http.Request) {
-	userID, err := s.getUserFromContextToken(r.Context())
-	if err != nil {
-		writeResponse(w, ErrorResponse{
-			Message: fmt.Sprintf("unable to create site: %v", err),
-			Status:  "error",
-		})
-		return
-	}
-	site := &pb.Site{}
-	err = json.NewDecoder(r.Body).Decode(site)
-	if err != nil {
-		return
-	}
-	if site.Url == "" {
-		http.Error(w, "url is not defined", http.StatusBadRequest)
-		return
-	}
-	_, err = s.hand.CreateSite(&models.Site{
-		URL:    site.Url,
-		UserID: fmt.Sprintf("%d", userID),
-	})
-	if err != nil {
-		http.Error(w, fmt.Sprintf("unable to create site: %v", err), http.StatusBadRequest)
-		return
-	}
-
-	resp := &AccountResponse{
-		CreatedTime: time.Now().UTC(),
-		ID:          "",
-	}
-
-	data, err := json.Marshal(resp)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("unable to marshal data: %v", err), http.StatusBadRequest)
-		return
-	}
-	fmt.Fprintf(w, string(data))
-	w.WriteHeader(http.StatusCreated)
-}
 
 func (s *server) makeHandlers() {
 	s.router.Group(func(r chi.Router) {
---
+++ internal/server/sites.go
@@ -0,0 +1,53 @@
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/saromanov/pinger/internal/models"
+	pb "github.com/saromanov/pinger/proto"
+)
+
+// createSite makes a new site for user
+func (s *server) createSite(w http.ResponseWriter, r *http.Request) {
+	userID, err := s.getUserFromContextToken(r.Context())
+	if err != nil {
+		writeResponse(w, ErrorResponse{
+			Message: fmt.Sprintf("unable to create site: %v", err),
+			Status:  "error",
+		})
+		return
+	}
+	site := &pb.Site{}
+	err = json.NewDecoder(r.Body).Decode(site)
+	if err != nil {
+		return
+	}
+	if site.Url == "" {
+		http.Error(w, "url is not defined", http.StatusBadRequest)
+		return
+	}
+	_, err = s.hand.CreateSite(&models.Site{
+		URL:    site.Url,
+		UserID: fmt.Sprintf("%d", userID),
+	})
+	if err != nil {
+		http.Error(w, fmt.Sprintf("unable to create site: %v", err), http.StatusBadRequest)
+		return
+	}
+
+	resp := &AccountResponse{
+		CreatedTime: time.Now().UTC(),
+		ID:          "",
+	}
+
+	data, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("unable to marshal data: %v", err), http.StatusBadRequest)
+		return
+	}
+	fmt.Fprintf(w, string(data))
+	w.WriteHeader(http.StatusCreated)
+}
